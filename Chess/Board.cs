b4670949f540e5c9851d4cae6628c288d094ca36
Ap0gy <alex.tennysondavies@gmail.com>
2023-03-20 17:59:23 +0000

    Add BoardDisplay

--- Chess/Board.cs
+++ Chess/Board.cs
@@ -8,11 +8,27 @@ public class Board
         Dictionary<Coords, Piece> chessBoard = new Dictionary<Coords, Piece>();
     }
     
-    public void DisplayBoard()
+    public static void DisplayBoard(Dictionary<Coords, Piece> locationCollection)
     {
+        for (var y = 7; y > -1; y--)
+        {
+            Console.Write("|");
+            for (var x = 0; x < 8; x++)
+            {
+                if ( locationCollection.ContainsKey(new Coords(x, y)))
+                {
+                    Console.Write("x");
+                }
+
+                if (!locationCollection.ContainsKey(new Coords(x, y)))
+                {
+                    Console.Write("-");
+                }
+                Console.Write("|");
+            }
+            Console.WriteLine();
+        }
     }
-    
-    
 }
 
 public record Coords(int x, int y)
