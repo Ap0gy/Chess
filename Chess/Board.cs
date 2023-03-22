f705bcb2a8b3f69e1dd27de1f464ea4e9764fb90
Alex Tennyson-Davies <alex.tennysondavies@gmail.com>
2023-03-22 00:07:44 +0000

    add move visualizer

--- Chess/Board.cs
+++ Chess/Board.cs
@@ -1,26 +1,58 @@
-﻿namespace Chess;
+﻿using Chess.Pieces;
 
-public class Board
+namespace Chess;
+
+
+public sealed class Board
 {
+    private static readonly Lazy<Board> lazy =
+        new Lazy<Board>(() => new Board());
+
+    public static Board Instance { get { return lazy.Value; } }
 
     private Board()
     {
         Dictionary<Coords, Piece> chessBoard = new Dictionary<Coords, Piece>();
     }
-    
-    public static void DisplayBoard(Dictionary<Coords, Piece> locationCollection)
+}
+
+public class BoardVisualizer
+{
+    public static void DisplayPieces(Dictionary<Coords, Piece> locationsCollection)
     {
         for (var y = 7; y > -1; y--)
         {
             Console.Write("|");
             for (var x = 0; x < 8; x++)
             {
-                if ( locationCollection.ContainsKey(new Coords(x, y)))
+                if (locationsCollection.ContainsKey(new Coords(x, y)))
                 {
                     Console.Write("x");
                 }
 
-                if (!locationCollection.ContainsKey(new Coords(x, y)))
+                if (!locationsCollection.ContainsKey(new Coords(x, y)))
+                {
+                    Console.Write("-");
+                }
+                Console.Write("|");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static void DisplayMoves(List<Coords> possibleMovesCollection)
+    {
+        for (var y = 7; y > -1; y--)
+        {
+            Console.Write("|");
+            for (var x = 0; x < 8; x++)
+            {
+                if (possibleMovesCollection.Contains(new Coords(x, y)))
+                {
+                    Console.Write("x");
+                }
+
+                if (!possibleMovesCollection.Contains(new Coords(x, y)))
                 {
                     Console.Write("-");
                 }
@@ -36,10 +68,6 @@ public record Coords(int x, int y)
 
 
 public record Piece(IPiece piece, Colour colour, Coords position)
-{
-}
-
-public interface IPiece
 {
 }
 
